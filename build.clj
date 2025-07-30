14e9e961759a17ff1ec65d49858da2815b88cc97
y2k <itwisterlx@gmail.com>
2025-07-31 00:38:36 +0200

    update deps format

--- build.clj
+++ build.clj
@@ -1,12 +1,11 @@
 (ns _ (:require [".github/vendor/make/0.3.0/main" :as m]))
 
 (m/build
- {:deps {:out-dir "src/vendor"
-         :deps ["android_app:0.1.0"
-                "chat_ui:0.2.0"
-                "effects:0.2.0"
-                "effects_tools:0.1.0"
-                "random:0.1.0"]}
+ {:deps ["android_app:0.1.0"
+         "chat_ui:0.2.0"
+         "effects:0.2.0"
+         "effects_tools:0.1.0"
+         "random:0.1.0"]
   :compile [{:target "eval"
              :src "build/manifest.clj"
              :out ".github/android/app/src/main/AndroidManifest.xml"}
